f134f27b7ad7603f2f7883aeed3ad9f6a154ba32
Harashavaradhan Edke <harashavaradhan.edke@gosite.com>
2023-12-31 00:49:16 +0530

    Echo endpoint

--- scripts/call_suggestions.sh
+++ scripts/call_suggestions.sh
@@ -3,6 +3,8 @@
 # Replace with the actual PR number and keywords
 pr_number=12345
 keywords="bug fix, feature request"
+echo "Endpoint in script:"
+ehco $SUGGESTION_ENDPOINT
 
 # Send the curl request
 curl -X POST -F "pr_number=$pr_number" -F "keywords=$keywords" "$SUGGESTION_ENDPOINT/"suggestions
