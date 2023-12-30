cef3fc5ec023ee466c9dab81e143bf94066fb144
Harashavaradhan Edke <harashavaradhan.edke@gosite.com>
2023-12-30 21:36:16 +0530

    Initial template

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Harashavaradhan Ashok Edke on 30/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
